26a811cc32b748e6f9e7756972fb6f04aa78a737
Ron Polenthon <dev@ron-polenthon.de>
2022-11-25 11:43:26 +0100

    Implemented MovementControl

---
+++ venv/init.py
@@ -0,0 +1,28 @@
+import asyncio
+
+from motor_control import MovementControl
+
+
+async def remote_car_control():
+    while True:
+        key = input()
+
+        if key == 'w':
+            print('forward')
+            my_movement_control.set_direction(0)
+        if key == 'a':
+            print('left')
+            my_movement_control.set_direction(-1)
+        if key == 'd':
+            print('right')
+            my_movement_control.set_direction(1)
+
+        await asyncio.sleep(0)
+
+
+my_movement_control = MovementControl()
+
+futures = [remote_car_control()]
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.wait(futures))
+loop.close()
---
+++ venv/motor_control.py
@@ -0,0 +1,44 @@
+import time
+import busio
+from board import SCL_1, SDA_1
+from adafruit_pca9685 import PCA9685
+from adafruit_servokit import ServoKit
+
+
+class MovementControl:
+    def __init__(self):
+        self.i2c = busio.I2C(SCL_1, SDA_1)
+        self.pca = PCA9685(self.i2c, address=0x40)
+        self.kit = ServoKit(channels=16, i2c=self.i2c)
+        self.motor = self.kit.continuous_servo[0]
+        self.servo = self.kit.servo[1]
+        self.setup_hardware()
+
+    def setup_hardware(self):
+        self.motor.throttle = 1
+        time.sleep(1 / 10)
+        self.motor.throttle = 0
+
+    def set_direction(self, direction):
+        if (direction >= -1.0) & (direction <= 1.0):
+            angle = 90
+            if direction < 0:
+                angle = 90 - (direction * -1.0 * 50)
+            if direction > 0:
+                angle = 90 + (direction * 50)
+            self.servo.angle = angle
+            print('Set angle', angle)
+
+    def set_speed(self, speed):
+        if (speed >= -1.0) & (speed <= 1.0):
+            print('Set throttle', speed)
+            if speed < 0:
+                self.motor.throttle = -1
+                time.sleep(1/10)
+                self.motor.throttle = 0
+                time.sleep(1/10)
+                self.motor.throttle = speed
+            if speed > 0:
+                self.motor = speed
+
+
