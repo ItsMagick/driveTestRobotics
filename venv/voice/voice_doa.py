ebe280d33eac6f90ed628ef2ee4527dba13492c3
Herbie <herbie@hof-university.de>
2022-12-19 15:50:09 +0100

    Working voice doa experiment

--- venv/voice/voice_doa.py
+++ venv/voice/voice_doa.py
@@ -1,4 +1,6 @@
-from tuning import Tuning
+#!/usr/bin/python3.8
+
+import tuning
 import usb.core
 import usb.util
 import time
@@ -6,11 +8,13 @@ import time
 dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
 
 if dev:
-    Mic_tuning = Tuning(dev)
-    print(Mic_tuning.direction)
+    Mic_tuning = tuning.Tuning(dev)
+    
     while True:
         try:
-            print(Mic_tuning.direction)
-            time.sleep(1)
+            if Mic_tuning.is_voice():
+                print(Mic_tuning.direction)
+            
+            # time.sleep(1)
         except KeyboardInterrupt:
             break
