26e1e5543054931a7d477acff5c3fc708c492aa8
Ron Polenthon <dev@ron-polenthon.de>
2022-12-28 13:36:36 +0100

    Voice Tests

--- venv/voice/voice_detection.py
+++ venv/voice/voice_detection.py
@@ -8,7 +8,6 @@ import argparse
 import struct
 from datetime import *
 from threading import Thread
-import pvporcupine
 from pvrecorder import PvRecorder
 
 class VoiceDetection:
@@ -19,14 +18,10 @@ class VoiceDetection:
         mic = usb.core.find(idVendor=0x2886, idProduct=0x0018)
         self.tuner = tuning.Tuning(mic)
 
-        self.porcupine = pvporcupine.create(
-            access_key='nzah8bgrstj0FbW+BTI7qeGM1qF3MTi+jmqtHfn69Q+gT7MKoxgRfg==',
-        )
         self.recorder = None
 
     def start(self):
-        print("Frame Length:", self.porcupine.frame_length)
-        self.recorder = PvRecorder(device_index=0, frame_length=self.porcupine.frame_length)
+        self.recorder = PvRecorder(device_index=0)
         self.recorder.start()
 
     def runObserver(self):
