2116c46b012c90c9f43d42482aff64ba895bfb10
Herbie <herbie@hof-university.de>
2023-01-02 22:45:32 +0100

    coco add

---
+++ venv/camera/coco_test.py
@@ -0,0 +1,68 @@
+from imageai.Detection import VideoObjectDetection
+import os
+from matplotlib import pyplot as plt
+import cv2
+import threading
+import time
+
+
+execution_path = os.getcwd()
+
+color_index = {'bus': 'red', 'handbag': 'steelblue', 'giraffe': 'orange', 'spoon': 'gray', 'cup': 'yellow', 'chair': 'green', 'elephant': 'pink', 'truck': 'indigo', 'motorcycle': 'azure', 'refrigerator': 'gold', 'keyboard': 'violet', 'cow': 'magenta', 'mouse': 'crimson', 'sports ball': 'raspberry', 'horse': 'maroon', 'cat': 'orchid', 'boat': 'slateblue', 'hot dog': 'navy', 'apple': 'cobalt', 'parking meter': 'aliceblue', 'sandwich': 'skyblue', 'skis': 'deepskyblue', 'microwave': 'peacock', 'knife': 'cadetblue', 'baseball bat': 'cyan', 'oven': 'lightcyan', 'carrot': 'coldgrey', 'scissors': 'seagreen', 'sheep': 'deepgreen', 'toothbrush': 'cobaltgreen', 'fire hydrant': 'limegreen', 'remote': 'forestgreen', 'bicycle': 'olivedrab', 'toilet': 'ivory', 'tv': 'khaki', 'skateboard': 'palegoldenrod', 'train': 'cornsilk', 'zebra': 'wheat', 'tie': 'burlywood', 'orange': 'melon', 'bird': 'bisque', 'dining table': 'chocolate', 'hair drier': 'sandybrown', 'cell phone': 'sienna', 'sink': 'coral', 'bench': 'salmon', 'bottle': 'brown', 'car': 'silver', 'bowl': 'maroon', 'tennis racket': 'palevilotered', 'airplane': 'lavenderblush', 'pizza': 'hotpink', 'umbrella': 'deeppink', 'bear': 'plum', 'fork': 'purple', 'laptop': 'indigo', 'vase': 'mediumpurple', 'baseball glove': 'slateblue', 'traffic light': 'mediumblue', 'bed': 'navy', 'broccoli': 'royalblue', 'backpack': 'slategray', 'snowboard': 'skyblue', 'kite': 'cadetblue', 'teddy bear': 'peacock', 'clock': 'lightcyan', 'wine glass': 'teal', 'frisbee': 'aquamarine', 'donut': 'mincream', 'suitcase': 'seagreen', 'dog': 'springgreen', 'banana': 'emeraldgreen', 'person': 'honeydew', 'surfboard': 'palegreen', 'cake': 'sapgreen', 'book': 'lawngreen', 'potted plant': 'greenyellow', 'toaster': 'ivory', 'stop sign': 'beige', 'couch': 'khaki'}
+
+
+resized = False
+
+def forSecond(frame2_number, output_arrays, count_arrays, average_count, returned_frame):
+
+    plt.clf()
+
+    this_colors = []
+    labels = []
+    sizes = []
+
+    counter = 0
+
+    for eachItem in average_count:
+        counter += 1
+        labels.append(eachItem + " = " + str(average_count[eachItem]))
+        sizes.append(average_count[eachItem])
+        this_colors.append(color_index[eachItem])
+
+    global resized
+
+    if (resized == False):
+        manager = plt.get_current_fig_manager()
+        manager.resize(width=1000, height=500)
+        resized = True
+
+    plt.subplot(1, 2, 1)
+    plt.title("Second : " + str(frame2_number))
+    plt.axis("off")
+    plt.imshow(returned_frame, interpolation="none")
+
+    plt.subplot(1, 2, 2)
+    plt.title("Analysis: " + str(frame2_number))
+    plt.pie(sizes, labels=labels, colors=this_colors, shadow=True, startangle=140, autopct="%1.1f%%")
+
+    plt.pause(0.01)
+
+
+def run_Camera():
+    camera = cv2.VideoCapture(2)
+
+    video_detector = VideoObjectDetection()
+    video_detector.setModelTypeAsTinyYOLOv3()
+    video_detector.setModelPath(os.path.join(execution_path, "venv/camera/tiny-yolov3.pt"))
+    video_detector.loadModel()
+
+    plt.show()
+
+    video_detector.detectObjectsFromVideo(camera_input=camera, frames_per_second=2, per_second_function=forSecond,  minimum_percentage_probability=30, return_detected_frame=True, log_progress=False, save_detected_video=False)
+
+
+_monitor_thread = threading.Thread(target=run_Camera, args=())
+_monitor_thread.daemon = True
+_monitor_thread.start()
+
+while(True): time.sleep(1)
